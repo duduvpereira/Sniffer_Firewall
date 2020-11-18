60d684e5c457db0b84dfdef288533bc97ea06620
Eduardo Pereira <71197054+duduvpereira@users.noreply.github.com>
2020-11-17 22:52:28 -0300

    Add files via upload

---
+++ recv.c
@@ -0,0 +1,193 @@
+/*-------------------------------------------------------------*/
+/* Exemplo Socket Raw - Captura pacotes recebidos na interface */
+/*-------------------------------------------------------------*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <linux/if_ether.h>
+#include <netpacket/packet.h>
+#include <net/ethernet.h>
+#include <signal.h>
+#include <net/if.h>
+/* Diretorios: net, netinet, linux contem os includes que descrevem */
+/* as estruturas de dados do header dos protocolos   	  	        */
+
+#include <net/if.h>  //estrutura ifr
+#include <netinet/ether.h> //header ethernet
+#include <netinet/in.h> //definicao de protocolos
+#include <arpa/inet.h> //funcoes para manipulacao de enderecos IP
+
+#include <netinet/in_systm.h> //tipos de dados
+
+#define BUFFSIZE 1518
+#define ETHERTYPE_LEN 2
+#define MAC_ADDR_LEN 6
+
+
+typedef unsigned char MacAddress[MAC_ADDR_LEN];
+extern int errno;
+// Atencao!! Confira no /usr/include do seu sisop o nome correto
+// das estruturas de dados dos protocolos.
+
+  unsigned char buff1[BUFFSIZE]; // buffer de recepcao
+  extern int errno;
+
+  int sockd;
+  int on;
+  struct ifreq ifr;
+
+
+int main(int argc,char *argv[])
+{
+
+  int retValue = 0;
+  char buffer[BUFFSIZE], dummyBuf[50];
+  struct sockaddr_ll destAddr;
+  short int etherTypeT = htons(0x0800);
+
+  /* Configura MAC Origem e Destino */
+  MacAddress destMac = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  MacAddress localMac = {0x00, 0x00, 0x00, 0x0a, 0x00, 0x04};
+
+
+
+    /* Criacao do socket. Todos os pacotes devem ser construidos a partir do protocolo Ethernet. */
+    /* De um "man" para ver os parametros.*/
+    /* htons: converte um short (2-byte) integer para standard network byte order. */
+    if((sockd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0) {
+       printf("Erro na criacao do socket.\n");
+       exit(1);
+    }
+
+	// O procedimento abaixo eh utilizado para "setar" a interface em modo promiscuo
+	strcpy(ifr.ifr_name, "eth0");
+	if(ioctl(sockd, SIOCGIFINDEX, &ifr) < 0)
+		printf("erro no ioctl!");
+	ioctl(sockd, SIOCGIFFLAGS, &ifr);
+	ifr.ifr_flags |= IFF_PROMISC;
+	ioctl(sockd, SIOCSIFFLAGS, &ifr);
+  int p=0;
+
+  //char ifname[IFNAMSIZ];
+  //strcpy(ifname, argv[1]);
+  //memset(&ifr, 0, sizeof (struct ifreq));
+	//strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
+	//if (ioctl(sockd, SIOCGIFINDEX, &ifr) < 0) {
+		//perror("SIOCGIFINDEX");
+		//exit(1);
+	//}
+
+
+
+  	struct ifreq if_idx;
+  	struct ifreq if_mac;
+  	struct sockaddr_ll socket_address;
+  	char ifname[IFNAMSIZ];
+  	int frame_len = 0;
+
+	strcpy(ifname, argv[1]);
+  /* Obtem o indice da interface de rede */
+  	memset(&if_idx, 0, sizeof (struct ifreq));
+  	strncpy(if_idx.ifr_name, ifname, IFNAMSIZ - 1);
+  	if (ioctl(sockd, SIOCGIFINDEX, &if_idx) < 0) {
+  		perror("SIOCGIFINDEX");
+  		exit(1);
+  	}
+
+  	/* Obtem o endereco MAC da interface local */
+  	memset(&if_mac, 0, sizeof (struct ifreq));
+  	strncpy(if_mac.ifr_name, ifname, IFNAMSIZ - 1);
+  	if (ioctl(sockd, SIOCGIFHWADDR, &if_mac) < 0) {
+  		perror("SIOCGIFHWADDR");
+  		exit(1);
+  	}
+
+  	/* Indice da interface de rede */
+  	socket_address.sll_ifindex = if_idx.ifr_ifindex;
+
+  	/* Tamanho do endereco (ETH_ALEN = 6) */
+  	socket_address.sll_halen = ETH_ALEN;
+
+  	/* Endereco MAC de destino */
+  	memcpy(socket_address.sll_addr, destMac, MAC_ADDR_LEN);
+
+
+
+
+
+
+
+
+  //destAddr.sll_family = htons(PF_PACKET);
+  //destAddr.sll_protocol = htons(ETH_P_ALL);
+  //destAddr.sll_halen = 6;
+  //destAddr.sll_ifindex = ifr.ifr_ifindex;  /* indice da interface pela qual os pacotes serao enviados. Eh necess�rio conferir este valor. */
+  //printf("%d\n", destAddr.sll_ifindex);;
+  //memcpy(&(destAddr.sll_addr), destMac, MAC_ADDR_LEN);
+  /* Indice da interface de rede */
+
+
+int t;
+	// recepcao de pacotes
+	while (1) {
+   		t = recv(sockd,(char *) &buff1, sizeof(buff1), 0x0);
+		// impress�o do conteudo - exemplo Endereco Destino e Endereco Origem
+		//printf("MAC Destino: %x:%x:%x:%x:%x:%x \n", buff1[0],buff1[1],buff1[2],buff1[3],buff1[4],buff1[5]);
+		//printf("MAC Origem:  %x:%x:%x:%x:%x:%x \n\n", buff1[6],buff1[7],buff1[8],buff1[9],buff1[10],buff1[11]);
+    if((buff1[12]==8)&&(buff1[13]==0)){
+      printf("IP Origem:  %d.%d.%d.%d \n\n", buff1[26],buff1[27],buff1[28],buff1[29]);
+      printf("IP Destino:  %d.%d.%d.%d \n\n", buff1[30],buff1[31],buff1[32],buff1[33]);
+      printf("Protocolo:  %d \n", buff1[23]);
+      p=(buff1[34]<<8)+(buff1[35]);
+      printf("Porta Origem:  %d \n", p);
+      p=(buff1[36]<<8)+(buff1[37]);
+      printf("Porta Destino:  %d \n\n", p);
+      //if((buff1[26] != 0x0a)&&(buff1[27] != 0x00)&&(buff1[28] != 0x00)&&
+      //(buff1[29] != 0x14)){
+        printf("Entrei\n");
+        buff1[0] = 0x00;
+        buff1[1] = 0x00;
+        buff1[2] = 0x00;
+        buff1[3] = 0xaa;
+        buff1[4] = 0x00;
+        buff1[5] = 0x00;
+        //if((retValue = sendto(sockd, buff1, 64, 0, (struct sockaddr *)&(destAddr), sizeof(struct sockaddr_ll))) < 0) {
+           //printf("ERROR! sendto() \n");
+           //exit(1);
+        //}
+
+      	buff1[6] = 0x00;
+        buff1[7] = 0x00;
+        buff1[8] = 0x00;
+        buff1[9] = 0xaa;
+        buff1[10] = 0x00;
+        buff1[11] = 0x04;
+
+
+
+
+
+        printf("%d\n", t);
+        printf("%d\n", sizeof(buff1));
+        printf("%x %x %x \n", buff1[42],buff1[43],buff1[44]);
+        printf("%d \n", buff1[23]);
+        if (sendto(sockd, buff1, t, 0, (struct sockaddr *)&socket_address, sizeof(struct sockaddr_ll)) < 0)
+		    {
+			        perror("send");
+			        close(sockd);
+			        exit(1);
+		    }
+
+
+        //}
+    }
+
+      }
+
+	}
